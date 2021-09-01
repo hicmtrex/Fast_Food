66437d58a0b7b90c189668836694706f4b872f04
Firstname Lastname <your_email@youremail.com>
2021-09-01 05:52:16 +0100

    first commit

--- src/components/Meals/AvaliableMeals.js
+++ src/components/Meals/AvaliableMeals.js
@@ -1,104 +1,50 @@
-import React, { useEffect,useState } from 'react'
-import Card from '../UI/Card';
-import MealItem from './MealItem/MealItem';
+import Card from "../UI/Card";
+import MealItem from "./MealItem/MealItem";
 
-import classes from './AvalibleMeals.module.css'
+import classes from "./AvalibleMeals.module.css";
 
 const DUMMY_MEALS = [
-    {
-      id: 'm1',
-      name: 'Sushi',
-      description: 'Finest fish and veggies',
-      price: 22.99,
-    },
-    {
-      id: 'm2',
-      name: 'Schnitzel',
-      description: 'A german specialty!',
-      price: 16.5,
-    },
-    {
-      id: 'm3',
-      name: 'Barbecue Burger',
-      description: 'American, raw, meaty',
-      price: 12.99,
-    },
-    {
-      id: 'm4',
-      name: 'Green Bowl',
-      description: 'Healthy...and green...',
-      price: 18.99,
-    },
-  ];
+  {
+    id: "m1",
+    name: "Sushi",
+    description: "Finest fish and veggies",
+    price: 22.99,
+  },
+  {
+    id: "m2",
+    name: "Schnitzel",
+    description: "A german specialty!",
+    price: 16.5,
+  },
+  {
+    id: "m3",
+    name: "Barbecue Burger",
+    description: "American, raw, meaty",
+    price: 12.99,
+  },
+  {
+    id: "m4",
+    name: "Green Bowl",
+    description: "Healthy...and green...",
+    price: 18.99,
+  },
+];
 
 export default function AvaliableMeals() {
-  const [meals, setMeals] = useState([]);
-  const [isLoading, setIsLoading] = useState(true);
-  const [httpError, setHttpError] = useState();
-  
-  useEffect(() => {
-    const fetchMeals = async () => {
-      
-     const response = await fetch('https://react-http-8ca7a-default-rtdb.europe-west1.firebasedatabase.app/meals.json') 
-      if (!response.ok) {
-          throw new Error('something went wrong!')
-        }
-      const respinseData = await response.json();
-      console.log(respinseData);
-
-      const loadedMeals = [];
-
-      for (const key in respinseData) {
-        loadedMeals.push({
-          id: key,
-          name: respinseData[key].name,
-          description: respinseData[key].description,
-          price: respinseData[key].price
-        });
-      }
-      setMeals(loadedMeals)
-      setIsLoading(false)
-    }
-
-  
-    fetchMeals().catch((error) => {
-      setIsLoading(false)
-      setHttpError(error.message) 
-       }); 
-   
-  },[])
-
-  if (isLoading) {
-    return (
-      <section className={classes.MealsLoading}>
-        <p>Loading...</p>
-      </section>
-    )
-  }
-
-  if (httpError) {
-    return (
-      <section className={classes.MealsError}>
-        <p>{httpError}</p>
+  const mealsList = DUMMY_MEALS.map((meal) => (
+    <MealItem
+      key={meal.id}
+      id={meal.id}
+      name={meal.name}
+      description={meal.description}
+      price={meal.price}
+    />
+  ));
+  return (
+    <section className={classes.meals}>
+      <Card>
+        <ul>{mealsList}</ul>
+      </Card>
     </section>
-    )
-  }
-    const mealsList = meals.map((meal) => (
-        <MealItem
-        key={meal.id}
-        id={meal.id}
-          name={meal.name}
-          description={meal.description}
-          price={meal.price}
-        />
-      ));
-    return (
-        <section className={classes.meals}>
-            <Card> 
-            <ul>
-            {mealsList}
-                </ul>
-                </Card>
-        </section>
-    )
+  );
 }
--- src/components/UI/Input.js
+++ src/components/UI/Input.js
@@ -1,13 +1,12 @@
-import React, { forwardRef } from 'react'
+import React from "react";
 
-import classes from './Input.module.css'
+import classes from "./Input.module.css";
 
-export default React.forwardRef(function Input(props,ref) {
-    return (
-        <div className={classes.input}>
-     <label htmlFor={props.input.id}>{props.label}</label>
-            <input ref={ref} {...props.input} />
-        </div>
-    )
-})
-
+export default React.forwardRef(function Input(props, ref) {
+  return (
+    <div className={classes.input}>
+      <label htmlFor={props.input.id}>{props.label}</label>
+      <input ref={ref} {...props.input} />
+    </div>
+  );
+});
